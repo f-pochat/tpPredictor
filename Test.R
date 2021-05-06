13537944ccc3c1ff403fb40e71d0b929475f34fc
fpochat02 <fpochat@losmolinos.edu.ar>
2021-05-06 09:21:25 -0300

    Push

--- Test.R
+++ Test.R
@@ -1,9 +1,28 @@
 #Test Git
 #Preuba
 #prueba mateo
-
+library(ggplot2)
+library(plotrix)
+library(dplyr)
 print("Hello world xd")
 print("hello xdxdxdxd")
 
 library(readxl)
 dataset <- read_excel("dataset.xlsx")
+View(dataset)
+
+angioplastia <- dataset[which(dataset$PROCEDIMIENTO == "ANGIOPLASTIA"),]
+cirugia <- dataset[which(dataset$PROCEDIMIENTO == "CIRUGIA"),]
+endovalvula <- dataset[which(dataset$PROCEDIMIENTO == "ENDOVALVULA"),]
+
+plot(factor(cirugia$EDAD),type = "h")
+cirugia %>%
+  ggplot( aes(x=EDAD))+
+  geom_density(fill="#b3ffb3", color="#66ff66", alpha=0.8)
+angioplastia %>%
+  ggplot( aes(x=EDAD))+
+  geom_density(fill="#b3ffb3", color="#66ff66", alpha=0.8)
+endovalvula %>%
+  ggplot( aes(x=EDAD))+
+  geom_density(fill="#b3ffb3", color="#66ff66", alpha=0.8)
+
